1f77bd1cb23459d2587883d0cbabf389dca0ba8a
fiolentanna <tigranka@gmail.com>
2021-03-30 02:11:44 +0300

    change card number to 5578121309626831

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
         public static void main(String[] args) {
             // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-            String number = "4916953355934634922";
+            String number = "5578121309626831";
             System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
         }
 
